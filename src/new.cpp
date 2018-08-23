2331e929bf0f56efde0632d9046fed76ac842bf0
I355414 <I355414@INLN56577721A.apj.global.corp.sap>
2018-08-23 11:19:08 +0530

    Inital Commit of Files

---
+++ src/new.cpp
@@ -0,0 +1,17 @@
+/*
+ * new.cpp
+ *
+ *  Created on: Aug 23, 2018
+ *      Author: I355414
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World in other file" << endl; // prints Hello World
+	return 0;
+}
+
+
